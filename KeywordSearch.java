bace7b1f32bad4dc916a7213db29fbb490ea19d4
pgold2 <144755945+pgold2@users.noreply.github.com>
2023-10-15 17:00:06 -0400

    Create KeywordSearch.java
    this is code can search through text files for keywords and give you the line the keyword is found

---
+++ KeywordSearch.java
@@ -0,0 +1,29 @@
+package MethodologyProject;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class KeywordSearch {
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\goldp\\OneDrive\\Documents\\semester 3 (Fall 23)\\Computer Methodology\\compMeth\\bin\\MethodologyProject\\ChocChipCookies.txt";
+    
+        String[] keywords = {"cookies ", "sugar", "flour"};
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                for (String keyword : keywords) {
+                    if (line.contains(keyword)) {
+                        // Keyword found in this line, process accordingly
+                        System.out.println("Line containing keyword '" + keyword + "': " + line);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+    }
+}
