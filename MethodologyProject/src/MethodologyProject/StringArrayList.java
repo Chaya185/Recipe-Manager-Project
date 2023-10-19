3af46d4212ae583260519c4621e2fca6a35934fa
Chayki Kanevski <126410874+Chaya185@users.noreply.github.com>
2023-10-19 18:13:34 -0400

    Update StringArrayList.java added comment

--- MethodologyProject/src/MethodologyProject/StringArrayList.java
+++ MethodologyProject/src/MethodologyProject/StringArrayList.java
@@ -1,3 +1,5 @@
+//This class is attempting to accept an array and turn it into an arrayList (to be used in Recipt object) to lessen the code for each hardcoded recipe object.
+//still doesnt work correctly with main and Recipe...to be figured out
 package MethodologyProject;
 
 import java.util.ArrayList;
