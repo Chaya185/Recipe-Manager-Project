e7329a6add8c1a8377e375f3b89dd1458a4a8fad
pgold2 <144755945+pgold2@users.noreply.github.com>
2023-10-23 01:47:51 -0400

    Update Main.java
    I made the pre-existing code modular. 
    user is now immediately prompted with a menu of what action he would like to do (find recipe or submit it). it then calls the correct method based off the valid choices. 
    I commented where certain methods should be created and accessed in the code.

--- MethodologyProject/src/MethodologyProject/Main.java
+++ MethodologyProject/src/MethodologyProject/Main.java
@@ -7,6 +7,30 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		menu();
+	}//closes main method 
+	
+	public static void menu () {
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Please enter your choice below: ");
+		System.out.println("1. find recipes \n2. submit recipes  ");
+		int choice = keyboard.nextInt();
+		
+		if (choice ==1) {
+			findRecipes(keyboard);
+		}
+		else if (choice ==2) {
+			submitRecipes(keyboard);
+		}
+		else {
+			System.out.println("\nERROR! INVALID CHOICE.");
+			menu();
+		}
+	}//closes menu 
+		
+		
+		public static void findRecipes(Scanner keyboard) {
 		//hardcode Recipe objects
 		// Create an Array with the ingredients
 		String[] recipeList1 = {"2 large eggs",
@@ -17,7 +41,7 @@ public class Main {
 				"1/4 cup shredded cheese (e.g., cheddar or mozzarella)",
 				"Cooking oil or butter for the pan"};
 
-		//call the method to turn the array of ingedients into an arrayList.
+		//call the method to turn the array of ingredients into an arrayList.
 		ArrayList ingredients1 = StringArrayList(recipeList1);
 
 		//create new recipe object passing in the recipe name, arrayList of ingredients and String of instructions
@@ -29,9 +53,19 @@ public class Main {
 				"6. Gently fold the other half of the omelet over the cheese and cook for a minute or two until the cheese melts.\n" +
 				"\n"));
 
+//need to add code that will find certain recipes in the array list and print them 
+		
+		}//closes findRecipes method 
+		
+		
+		
+		
+		public static void submitRecipes(Scanner keyboard) {
 		String name;
 		int number;
-		Scanner keyboard = new Scanner(System.in);
+		
+		//consume buffer
+		keyboard.nextLine();
 		  System.out.println("Please enter the name of the recipe: ");
 			name=keyboard.nextLine();
 			
@@ -61,10 +95,16 @@ public class Main {
 		//add the recipe to list of recipes using recipeManager
 		RecipeManager addRecipeObj = new RecipeManager();
 			addRecipeObj.addRecipe(recipe);
-	
-}
+			
+			System.out.println("recipe is getting submitted!");
+			
+//need code here that will access a recipe method that adds this newly created recipe to the recipe array list 
+			
+			
+		}//closes submitRecipes method 
 
-	//this method accepts an array of strings, coverts it to an arrayList and return the arrayList
+
+	//this method accepts an array of strings, converts it to an arrayList and return the arrayList if prompted AKA what the find recipe method is technically doing. 
 	//method is used to create hardcoded recipes
 	public static ArrayList StringArrayList(String[] stringArray) {
 		ArrayList<String> stringList;
@@ -74,5 +114,7 @@ public class Main {
 		}
 		return stringList;
 	}
+		
 
-}
+}//closes class
+		
