3d190bb7a8bb5e2f355ef2f392c86dc5ea90e040
Chaya Kanevski <ckanevsk@student.touro.edu>
2023-10-23 17:03:16 -0400

    added method to search the array for requested recipe

--- MethodologyProject/src/MethodologyProject/RecipeManager.java
+++ MethodologyProject/src/MethodologyProject/RecipeManager.java
@@ -21,4 +21,14 @@ public class RecipeManager {
         recipes.remove(recipe);
     }
 
+    //method to retrieve recipe based on the name of the recipe passed in
+    public Recipe getRecipeByName(String recipeName) {
+        for (Recipe recipe : recipes) {
+            if (recipe.getName().equalsIgnoreCase(recipeName)) {
+                return recipe;
+            }
+        }
+        return null; // Recipe not found
+    }
+
 }
