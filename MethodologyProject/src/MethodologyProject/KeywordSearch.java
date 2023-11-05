12b6638b4f38dc1dd4ab58ecfdd4695d6b6acb20
pgold2 <144755945+pgold2@users.noreply.github.com>
2023-11-05 14:35:37 -0500

    Update KeywordSearch.java
    now this will print the whole recipe based off of a searched keyword

--- MethodologyProject/src/MethodologyProject/KeywordSearch.java
+++ MethodologyProject/src/MethodologyProject/KeywordSearch.java
@@ -10,18 +10,31 @@ public class KeywordSearch {
     public static void main(String[] args) {
         String filePath = "C:\\Users\\goldp\\OneDrive\\Documents\\semester 3 (Fall 23)\\Computer Methodology\\compMeth\\bin\\MethodologyProject\\ChocChipCookies.txt";
     
-        String[] keywords = {"cookies ", "sugar", "flour"};
-
+        String[] keywords = {"Pizza"};
+        String breakline = "STOP";
+        boolean shouldPrint = false;
+        
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
-            while ((line = br.readLine()) != null) {
-                for (String keyword : keywords) {
-                    if (line.contains(keyword)) {
-                        // Keyword found in this line, process accordingly
-                        System.out.println("Line containing keyword '" + keyword + "': " + line);
-                    }
+            
+            while ((line = br.readLine()) != null){// && !line.contains(breakline)) {
+            	
+            	if (line.contains(breakline)) {
+                    // Found the "STOP" keyword, stop printing
+                    shouldPrint = false;
                 }
-            }
+            	 for (String keyword : keywords) {
+                     if (line.contains(keyword)) {
+                         // Found the "cookies" keyword, start printing
+                         shouldPrint = true;
+                     }
+                 }
+            	
+            	 if (shouldPrint) {
+                     System.out.println(line);
+                 }
+            	 
+                }
         } catch (IOException e) {
             System.err.println("Error reading the file: " + e.getMessage());
         }
