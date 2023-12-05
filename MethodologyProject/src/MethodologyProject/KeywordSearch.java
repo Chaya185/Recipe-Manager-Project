cd9a7d8f723640c2ac260922852392810483bd17
scousin2 <126409148+scousin2@users.noreply.github.com>
2023-12-04 20:37:44 -0500

    Update KeywordSearch.java
    able to search for recipe.

--- MethodologyProject/src/MethodologyProject/KeywordSearch.java
+++ MethodologyProject/src/MethodologyProject/KeywordSearch.java
@@ -3,40 +3,70 @@ package MethodologyProject;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class KeywordSearch {
-    public static void main(String[] args) {
-        String filePath = "C:\\Users\\goldp\\OneDrive\\Documents\\semester 3 (Fall 23)\\Computer Methodology\\compMeth\\bin\\MethodologyProject\\ChocChipCookies.txt";
-    
-        String[] keywords = {"Pizza"};
-        String breakline = "----";
-        boolean shouldPrint = false;
-        
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
+    public static void main(String[] args) throws FileNotFoundException {
+       //String filePath = "/Users/saracousin/git/MethodologyProject/MethodologyProject/src/MethodologyProject/ChocChipCookies";
+    	//try {
+            File myFile = new File("Recipes.txt");
             
-            while ((line = br.readLine()) != null){// && !line.contains(breakline)) {
-            	
-            	if (line.contains(breakline)) {
-                    // Found the "STOP" keyword, stop printing
-                    shouldPrint = false;
-                }
-            	 for (String keyword : keywords) {
-                     if (line.contains(keyword)) {
-                         // Found the "cookies" keyword, start printing
-                         shouldPrint = true;
-                     }
-                 }
-            	
-            	 if (shouldPrint) {
-                     System.out.println(line);
-                 }
-            	 
-                }
-        } catch (IOException e) {
-            System.err.println("Error reading the file: " + e.getMessage());
-        }
+            Scanner fileScanner = new Scanner(myFile);
+           // int numWinners = 104;
+            //create the array
+           // String[] names = new String[numWinners]; 
+    	
+            // while loop to set the array values
+        // int i = 0;
+    	//while (fileScanner.hasNextLine() && i < names.length) {
+        	//names[i] = fileScanner.nextLine();
+        	//i++;
+            searchForRecipesInFile(myFile);
     }
-}
+
+	private static void searchForRecipesInFile(File myFile) {
+		String[] keywords = AskRecipe();
+      String breakline = "---";
+      boolean shouldPrint = false;
+      
+      Printrecipe(myFile, keywords, breakline, shouldPrint);
+	}
+
+	private static void Printrecipe(File myFile, String[] keywords, String breakline, boolean shouldPrint) {
+		try (BufferedReader br = new BufferedReader(new FileReader(myFile))) {
+			String line;
+			
+			while ((line = br.readLine()) != null) {
+				
+				if (line.contains(breakline)) {
+			        // Found the "STOP" keyword, stop printing
+			        shouldPrint = false;
+			    }
+				 for (String keyword : keywords) {
+			         if (line.equalsIgnoreCase(keyword)) {
+			             // Found the "cookies" keyword, start printing
+			             shouldPrint = true;
+			         }
+			     }
+				
+				 if (shouldPrint) {
+			         System.out.println(line);
+			     }
+				 
+			    }
+		  } catch (IOException e) {
+			System.err.println("Error reading the file: " + e.getMessage());
+		  }
+	}
+
+	private static String[] AskRecipe() {
+		Scanner scanner = new Scanner(System.in);
+      System.out.println("what recipe would you like to look for?");	
+      String recipeRequest = scanner.nextLine();
+      String[] keywords = {recipeRequest};
+		return keywords;
+	}
+} 
