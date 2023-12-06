ead0ab2e791bc9b1d3c09b34757b65f58baace51
rsteinberg6 <rsteinbe6@student.touro.edu>
2023-12-05 20:03:34 -0500

    fixed an error

--- MethodologyProject/src/MethodologyProject/Main.java
+++ MethodologyProject/src/MethodologyProject/Main.java
@@ -27,7 +27,7 @@ public class Main {
 		//do while loop to display menu so long as user wants to repeat menu options
 		String repeat;
 		do {
-		//ADD CODE TO display list of recipes
+//ADD CODE TO display list of recipes
 			menu(keyboard, recipeManager, fwriter, listRecipes);
 			System.out.println("Would you like to view menu again?  ('yes/no') ");
 			repeat = keyboard.nextLine();
@@ -47,8 +47,8 @@ public class Main {
 		if (choice == 1) {
 			System.out.println();
 			getRecipeFromList(keyboard, recipeManager);
-			//KeywordSearch key = new KeywordSearch();
-	    	//KeywordSearch.main(null);
+			KeywordSearch key = new KeywordSearch();
+	    	KeywordSearch.main(null);
 	    	keyboard.nextLine();
 			
 			//findRecipes(keyboard, recipeManager);
@@ -112,35 +112,33 @@ public class Main {
 
 		//call the method to turn the array of recipes into an arrayList.
 		//ArrayList listOfRecipes = StringArrayList(listRecipes);
-		//need to add code that will find certain recipes in the array list and print them
+
+
+
+//need to add code that will find certain recipes in the array list and print them
 		
 		}//closes findRecipes method
 
 	private static Recipe getRecipeFromList(Scanner keyboard, RecipeManager recipeManager) throws FileNotFoundException {
-		// Read recipes from the file and update listRecipes
-		try (Scanner fileScanner = new Scanner(new File("Recipes"))) {
-			while (fileScanner.hasNextLine()) {
-				String recipeName = fileScanner.nextLine().trim();
-				if (!listRecipes.contains(recipeName)) {
-					listRecipes.add(recipeName);
-				}
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found: Recipes");
-		}
+		//add the names of the recipes to an array list
 
-		// Display the recipes
-		System.out.println("Choose from the available recipes: ");
-		for (String recipe : listRecipes) {
-			System.out.println(recipe);
-		}
+		ArrayList<String> listRecipes = new ArrayList<>();
+		listRecipes.add("Omelet");
+		listRecipes.add("Pasta");
+		listRecipes.add("Chicken");
+		listRecipes.add("Smoothie");
+		listRecipes.add("Biscotti");
 
-		System.out.println("Enter the name of the recipe: ");
+		// Using a for loop to display the strings in the array
+		for (int i = 0; i < listRecipes.size(); i++) {
+			System.out.println(listRecipes.get(i));
+		}
+		keyboard.nextLine();
+		System.out.println("Choose from the above recipes: (type the word) ");
 		String choice = keyboard.nextLine();
 		Recipe requestedRecipe = recipeManager.getRecipeByName(choice);
 		return requestedRecipe;
 	}
-
 
 
 	public static void submitRecipes(Scanner keyboard, FileWriter fwriter, ArrayList<String> listRecipes) throws IOException {
@@ -152,7 +150,7 @@ public class Main {
 		keyboard.nextLine();
 		  System.out.println("Please enter the name of the recipe: ");
 			name=keyboard.nextLine();
-			Main.listRecipes.add(name);
+			listRecipes.add(name);
 			
 			System.out.println("How many ingredients does this recipe require?");
 			number=keyboard.nextInt();
@@ -188,12 +186,148 @@ public class Main {
 
 		listRecipes.add(name);
 
+
+
 		System.out.println("recipe is getting submitted!");
 			
 //need code here that will access a recipe method that adds this newly created recipe to the recipe array list
 
 
 		}//closes submitRecipes method 
+
+/*
+	public static void createRecipeList(RecipeManager recipeManager) {
+		//hardcode Recipe objects
+
+
+		//OMELET
+		// Create an Array with the ingredients
+		String[] omeletIngredients = {"2 large eggs",
+				"Salt and pepper to taste",
+				"1/4 cup diced bell peppers",
+				"1/4 cup diced onions",
+				"1/4 cup diced tomatoes",
+				"1/4 cup shredded cheese (e.g., cheddar or mozzarella)",
+				"Cooking oil or butter for the pan"};
+
+		//call the method to turn the array of ingredients into an arrayList.
+		ArrayList ingredients1 = StringArrayList(omeletIngredients);
+		Recipe omelet = new Recipe("Omelet", ingredients1, ("1. In a bowl, whisk the eggs and season with salt and pepper.\n" +
+				"2. Heat a non-stick skillet over medium heat and add a little oil or butter.\n" +
+				"3. Add the diced vegetables and sauté until they are tender.\n" +
+				"4. Pour the whisked eggs over the vegetables and cook, swirling the pan to spread the eggs evenly.\n" +
+				"5. When the edges start to set, sprinkle the shredded cheese on one half of the omelet.\n" +
+				"6. Gently fold the other half of the omelet over the cheese and cook for a minute or two until the cheese melts.\n" +
+				"\n"));
+
+		//add the recipe to the list of recipes using recipeManager class
+		recipeManager.addRecipe(omelet);
+
+
+		//PASTA
+		// Create an Array with the ingredients
+		String[] pastaIngredients = {"1 box of pasta",
+				"Salt to taste",
+				"1/4 cup mozzarella cheese",
+				"1/4 cup marinara sauce",
+				"1 tsp butter",
+				"2 tbs milk ",
+				"1/2 tsp red pepper flakes",
+				"1/2 tsp basil"};
+
+		//call the method to turn the array of ingredients into an arrayList.
+		ArrayList ingredients2 = StringArrayList(pastaIngredients);
+
+		//create new recipe object passing in the recipe name, arrayList of ingredients and String of instructions
+		Recipe pasta = new Recipe("Pasta", ingredients2, ("1. Boil a pot of water.\n" +
+				"2. Add some salt and once boiled, add your pasta.\n" +
+				"3. Cook pasta for about 11 minutes on medium flame.\n" +
+				"4. Once cooked, drain your pasta and pour cold water over it.\n" +
+				"5. Now add your marinara sauce, cheese, butter, milk, red pepper flakes, basil and salt.\n" +
+				"6. Mix until the cheese melts.\n" +
+				"\n"));
+
+		//add the recipe to the list of recipes using recipeManager class
+		recipeManager.addRecipe(pasta);
+
+		//CHICKEN
+		// Create an Array with the ingredients
+		String[] chickenIngredients = {"4 boneless chicken breasts",
+				"2 cups crushed pretzels",
+				"1 cup all purpose flour",
+				"2 eggs, beaten",
+				"Salt and Pepper to taste",
+				"cooking oil",
+				"cooked rice for serving"};
+
+		//call the method to turn the array of ingredients into an arrayList.
+		ArrayList ingredients3 = StringArrayList(chickenIngredients);
+
+		//create new recipe object passing in the recipe name, arrayList of ingredients and String of instructions
+		Recipe chicken = new Recipe("Chicken", ingredients3, ("1. Preheat your oven to 375 F.\n"+
+				"2. Season the chicken breasts with salt and pepper\n"+
+				"3. Dredge each chicken breast in flour, dip in eggs, and coat with crushed pretzels\n" +
+				"4. Heat some cooking oil in a large oven-safe skillet over medium-high heat.\n"+
+				"5. Add the chicken breasts and cook for 2-3 minutes on each side, or until they are golden brown.\n" +
+				"6. Transfer the skillet to the preheated oven and bake for about 20 minutes, or until the chicken is cooked through.\n" +
+				"7. Serve the pretzel-crusted chicken over cooked rice.\n" +
+				"\n"));
+
+		//add the recipe to the list of recipes using recipeManager class
+		recipeManager.addRecipe(chicken);
+
+		//SMOOTHIE
+		// Create an Array with the ingredients
+		String[] smoothieIngredients = {"1 mango, skin removed, cut into 1-inch dice, frozen\n" +
+				"5 strawberries, cut half, frozen\n" +
+				"1 cup diced pineapple\n" +
+				"1 cup orange juice" };
+
+		//call the method to turn the array of ingredients into an arrayList.
+		ArrayList ingredients4 = StringArrayList(smoothieIngredients);
+
+		//create new recipe object passing in the recipe name, arrayList of ingredients and String of instructions
+		Recipe smoothie = new Recipe("Smoothie", ingredients4, ("Combine all ingredients in a blender and puree until smooth, about 2 to 3 minutes.\n" +
+				"Drink immediately.\n") );
+
+		//add the recipe to the list of recipes using recipeManager class
+		recipeManager.addRecipe(smoothie);
+
+		//BISCOTTI
+		// Create an Array with the ingredients
+		String [] biscottiIngredients = {"2 cups all-purpose flour\n" +
+				"1 1/2 teaspoons baking powder\n" +
+				"1/4 teaspoon salt\n" +
+				"1/2 cup unsalted butter, softened\n" +
+				"1 cup granulated sugar\n" +
+				"2 large eggs\n" +
+				"1 teaspoon vanilla extract\n" +
+				"1 cup chopped nuts or dried fruits (e.g., almonds, pistachios, cranberries, or chocolate chips)"};
+
+		//call the method to turn the array of ingredients into an arrayList.
+		ArrayList ingredients5 = StringArrayList(biscottiIngredients);
+
+		//create new recipe object passing in the recipe name, arrayList of ingredients and String of instructions
+		Recipe biscotti = new Recipe("Biscotti", ingredients5, ("1. Preheat your oven to 350°F (175°C) and line a baking sheet with parchment paper.\n" +
+				"2. In a bowl, whisk together the flour, baking powder, and salt.\n" +
+				"3. In another bowl, beat the softened butter and sugar together until light and fluffy.\n" +
+				"4. Beat in the eggs one at a time, followed by the vanilla extract.\n" +
+				"5. Gradually add the dry ingredients and mix until well combined.\n" +
+				"6. Stir in the nuts or dried fruits.\n" +
+				"7. Divide the dough in half and shape each half into a log, about 12 inches long.\n" +
+				"8. Place the logs on the prepared baking sheet and bake for about 25-30 minutes or until they are firm to the touch.\n" +
+				"9. Let the logs cool for a few minutes, then slice them into biscotti about 1/2 inch thick.\n" +
+				"10. Lay the biscotti on their sides and bake for an additional 10-12 minutes or until they're golden brown and crisp.\n" +
+				"11. Allow the biscotti to cool completely before enjoying with your favorite hot beverage.\n" +
+				"\n"));
+
+		//add the recipe to the list of recipes using recipeManager class
+		recipeManager.addRecipe(biscotti);
+
+	}//closes the method create recipe list
+
+*/
+
 
 	//this method accepts an array of strings, converts it to an arrayList and return the arrayList if prompted AKA what the find recipe method is technically doing.
 	//method is used to create hardcoded recipes
